7dc29c3476dee0d71e4e11e53d16b920e3cb25bc
Edward Paler <paler.edward@gmail.com>
2021-11-10 14:14:02 +0800

    Add activity code

--- index.js
+++ index.js
@@ -41,7 +41,43 @@ const showPosts = (posts) => {
     document.querySelector('#div-post-entries').innerHTML = postEntries;
 }
 //Edit post
+const editPost = (id) => {
+    let title = document.querySelector(`#post-title-${id}`).innerHTML;
+    let body = document.querySelector(`#post-body-${id}`).innerHTML;
 
+    document.querySelector('#txt-edit-id').value = id;
+    document.querySelector('#txt-edit-title').value = title;
+    document.querySelector('#txt-edit-body').value = body;
+    document.querySelector('#btn-submit-update').removeAttribute('disabled');
+}
 //Update post
+document.querySelector('#form-edit-post').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'PUT',
+        body: JSON.stringify({
+            id: document.querySelector('#txt-edit-id').value,
+            title: document.querySelector('#txt-edit-title').value,
+            body: document.querySelector('#txt-edit-body').value,
+            userId: 1
+        }),
+        headers: {'Content-type': 'application/json; charset=UTF-8'}
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        alert('Successfully updated.');
+
+        document.querySelector('#txt-edit-id').value = null;
+        document.querySelector('#txt-edit-title').value = null;
+        document.querySelector('#txt-edit-body').value = null;
+        document.querySelector('#btn-submit-update').setAttribute('disabled', true);
+    })
+})
 
 //Delete post
+
+const deletePost = (id) => {
+    document.querySelector(`#post-${id}`).remove();
+}
